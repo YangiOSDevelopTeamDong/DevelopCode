f99fa5c283e24d7fc11718b3d03d424f23dd9e23
yanglirong <yanglirong_sky123@163.com>
2015-08-25 00:16:38 +0800

    修改了背景颜色

--- 手势解锁/ViewController.m
+++ 手势解锁/ViewController.m
@@ -19,7 +19,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor blackColor];
     
     
     //1 默认排版
