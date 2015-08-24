629c3bfbe7aa3f9a8ad4d210a11de8677bb48294
yanglirong <yanglirong_sky123@163.com>
2015-08-24 23:55:52 +0800

    shsf

--- 手势解锁/ViewController.m
+++ 手势解锁/ViewController.m
@@ -19,10 +19,6 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    
-    
-    
     self.view.backgroundColor = [UIColor redColor];
     //1 默认排版
     
